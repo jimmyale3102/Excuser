bbfb39f60620abcb0fbcf5b66cfec0bf81a79744
jimmyale3102 <jimmyale3102@gmail.com>
2023-06-19 16:31:39 +1000

    Add spanish excuses for family

---
+++ Excuses_es.js
@@ -0,0 +1,104 @@
+module.exports = [
+    // ---------- Family ----------
+    {
+        id: 1,
+        excuse: "Mi mejor amigo llegó tarde al trabajo y tuve que reemplazarlo.",
+        category: "family",
+    },
+    {
+        id: 2,
+        excuse: "Mi jefe estaba en una reunión y tuve que ocupar su lugar.",
+        category: "family",
+    },
+    {
+        id: 3,
+        excuse: "I got involved in a fight",
+        excuse: "Tuve una pelea.",
+        category: "family",
+    },
+    {
+        id: 4,
+        excuse: "Se me dañó el carro.",
+        category: "family",
+    },
+    {
+        id: 5,
+        excuse: "No encuentro mis llaves",
+        category: "family",
+    },
+    {
+        id: 6,
+        excuse: "Tengo un dolor de estómago muy fuerte.",
+        category: "family",
+    },
+    {
+        id: 7,
+        excuse: "Mi mejor amigo tuvo un accidente y tuve reemplazarlo.",
+        category: "family",
+    },
+    {
+        id: 8,
+        excuse: "Mi jefe acaba de llamarme diciendo que una reunión de última hora.",
+        category: "family",
+    },
+    {
+        id: 9,
+        excuse: "¿Era hoy? Lo siento mucho chicos, pero le prometí a mi novia que saldríamos esta noche.",
+        category: "family",
+    },
+    {
+        id: 10,
+        excuse: "La mamá de mi compañero falleció",
+        category: "family",
+    },
+    {
+        id: 11,
+        excuse: "Mañana tengo que trabajar tempreano.",
+        category: "family",
+    },
+    {
+        id: 12,
+        excuse: "Honestamente, preferiría quedarme en la casa y leer.",
+        category: "family",
+    },
+    {
+        id: 13,
+        excuse: "No cuenten conmigo para la fiesta más tarde. Llevo toda la mañana vomitando.",
+        category: "family",
+    },
+    {
+        id: 14,
+        excuse: "Iba a salir en el carro, pero me di cuenta que tenía una llanta pinchada.",
+        category: "family",
+    },
+    {
+        id: 15,
+        excuse: "La niñera me canceló a última hora.",
+        category: "family",
+    },
+    {
+        id: 16,
+        excuse: "Está lloviendo muy fuerte. No creo que sea buena idea manejar así.",
+        category: "family",
+    },
+    {
+        id: 17,
+        excuse: "Estoy en semana de vacaciones y tengo que ponerme a estudiar.",
+        category: "family",
+    },
+    {
+        id: 18,
+        excuse: "La empresa de taxis me dijo que no habrá disponibilidad durante una hora.",
+        category: "family",
+    },
+    {
+        id: 19,
+        excuse: "Me encantaría haber salido esta noche, pero estoy buscando mi billetera.",
+        category: "family",
+    },
+    {
+        id: 20,
+        excuse: "Hay un problema de drenaje en mi casa y estoy esperando a mi fontanero.",
+        category: "family",
+    },
+];
